52e47c1a28f5c9a7d73670f6ee3edc3405e425d8
mounica <mounicagrandhi96@gmail.com>
2019-09-20 17:19:05 +0530

    code

--- add2numbers.js
+++ add2numbers.js
@@ -1,9 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 function sum() {
-    var x = 90;
-    var y = 109;
-    z = x + i;
-    console.log('Sum is'+z)
+  z = x + y;
+  console.log('Sum is'+z)
 }
 return sum()
